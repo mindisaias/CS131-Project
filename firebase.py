a7d34e45de31fff744b229ce61ec9ded159558cc
Willow <isaiasnoel@gmail.com>
2025-06-01 12:08:07 -0700

    moved mock test data to if name = main

--- firebase.py
+++ firebase.py
@@ -7,15 +7,6 @@ firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://cs131-smart-light-default-rtdb.firebaseio.com/'
 })
 
-data = {
-    "device": "raspberry pi", #What is capturing the data?
-    "room": "bedroom", #Where is the data being captured?
-    "motion": True, #Are PIR Sensors detecting motion
-    "lux": 73, #Represents the ambient light level
-    "led_status": "ON", #Is the light on?
-    "color": "Red", #What color is our light?
-    "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S") #When this data was logged
-}
 def push_data_to_firebase(data):
     #Real Time Data
     try:
